11bb127204475842884dbf3d0b1b63dc02ef378d
Demonz <demonz@Demonzs-MacBook-Air.local>
2023-04-30 02:33:51 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+
+   $employees = collect([
+            [
+                'name' => 'John',
+                'email' => 'john3@example.com',
+                'sales' => [
+                    ['customer' => 'The Blue Rabbit Company', 'order_total' => 7444],
+                    ['customer' => 'Black Melon', 'order_total' => 1445],
+                    ['customer' => 'Foggy Toaster', 'order_total' => 700],
+                ],
+            ],
+            [
+                'name' => 'Jane',
+                'email' => 'jane8@example.com',
+                'sales' => [
+                    ['customer' => 'The Grey Apple Company', 'order_total' => 203],
+                    ['customer' => 'Yellow Cake', 'order_total' => 8730],
+                    ['customer' => 'The Piping Bull Company', 'order_total' => 3337],
+                    ['customer' => 'The Cloudy Dog Company', 'order_total' => 5310],
+                ],
+            ],
+            [
+                'name' => 'Dave',
+                'email' => 'dave1@example.com',
+                'sales' => [
+                    ['customer' => 'The Acute Toaster Company', 'order_total' => 1091],
+                    ['customer' => 'Green Mobile', 'order_total' => 2370],
+                ],
+            ],
+        ]);
+
+
+$employee = [
+        function ($collection) {
+            return $collection->map(function ($employee) {
+                $totalSales = collect($employee['sales'])->sum('order_total');
+                return array_merge($employee, ['total_sales' => $totalSales]);
+            });
+        },
+        function ($collection) {
+            return $collection->sortByDesc('total_sales');
+        },
+        function ($collection) {
+            return $collection->first();
+        }
+    ];
+
+    $mostValuableSaleEmployee = $employees->pipeThrough($employee);
+
+    return view('welcome',['mostValuableSaleEmployee' => $mostValuableSaleEmployee]);
+});
+
